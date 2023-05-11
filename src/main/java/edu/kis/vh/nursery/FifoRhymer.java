e4f416296af9dd8832d8740cd0ae2c32eac22111
studencik997 <235891@edu.p.lodz.pl>
2023-05-11 17:46:32 +0200

    17. Wygenerowanie szkieletu dokumentacji

--- src/main/java/edu/kis/vh/nursery/FifoRhymer.java
+++ src/main/java/edu/kis/vh/nursery/FifoRhymer.java
@@ -1,7 +1,11 @@
 package edu.kis.vh.nursery;
 
 public class FifoRhymer extends DefaultCountingOutRhymer {
-
+    /**
+     * In the countOut method elements are taken from the stack of parent class and added
+     * to the temporary stack. Then the elements are taken from the temporary stack and added back to the stack
+     * of the parent class so that the order of the elements on it matches the order at the beginning.
+     */
     private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
     @Override
